97fdebc7117ac4fdbece7f48db409254eafacf30
vyapariapp <midhilajm18@gmail.com>
2020-05-20 16:21:07 +0530

    Alert box to confirm delete code added

--- src/app/component-item/component-item.component.ts
+++ src/app/component-item/component-item.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core'; 
 import {  Input,Output,EventEmitter} from '@angular/core'; 
 import {ComponentItem} from '../model/componentitem';
-import DeletingConfirmationBoxComponent  from '../deleting-confirmation-box/deleting-confirmation-box.component';
-
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatDialog} from '@angular/material/dialog';
+ 
+import { DeletingConfirmationBoxComponent } from "/~/src/app/deleting-confirmation-box/deleting-confirmation-box.component"
+ 
 @Component({
   selector: 'app-component-item',
   templateUrl: './component-item.component.html',
@@ -15,22 +14,25 @@ export class ComponentItemComponent implements OnInit {
   @Input() data:ComponentItem;
   @Output() onClickOnDelete: EventEmitter<String> = new EventEmitter<any>();
 
-  constructor(public dialog: MatDialog) {}
+ 
 
   ngOnInit() {
   }
 
   deleteMe(index){
-    this.onClickOnDelete.emit(index);
+   this.askToComfirmDialog(index);
 
   }
 
-  askToComfirmDialog() {
-    const dialogRef = this.dialog.open(DeletingConfirmationBoxComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+  askToComfirmDialog(index) {
+   
+  var r = confirm("Are you sure\n You want to delete this component");
+  if (r == true) {
+   this.onClickOnDelete.emit(index);
+  } else {
+    
+  }
+    
   }
 
 }
